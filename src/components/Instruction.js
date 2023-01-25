29a780b0546caad9d82b1a21a6baa27109879c1c
RicardMarkRoss <cptrichkidprice@gmail.com>
2023-01-25 16:32:34 +0200

    colum,s

--- src/components/Instruction.js
+++ src/components/Instruction.js
@@ -39,19 +39,18 @@ export class Instruction extends Component {
       return <div>Loading...</div>;
     } else {
     return(
-      <div>
-      {items.map((item) => ( 
-        <div class="card">
-        <img class="card-img-top" src= {item.image} alt="Card image cap"/>
-        <div class="card-body">
-          <h5 class="card-title">{item.title}</h5>
-          <p class="card-text"> {item.description}</p>
-          <a href="#" class="btn btn-primary">{item.price}</a>
-          </div>
+        <div class="card-columns">
+        {items.map((item) => ( 
+                <div class="card">
+                <img class="card-img-top" src= {item.image} alt="Card image cap"/>
+                <div class="card-body">
+                  <h5 class="card-title">{item.title}</h5>
+                  <p class="card-text"> {item.description}</p>
+                  <a href="#" class="btn btn-primary">R {item.price.toFixed(2)}</a>
+                  </div>
+                </div>
+              ))}
         </div>
-        
-      ))}
-      </div>
     )
   }
 }
